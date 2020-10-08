eaefa5af898420c69b6099b58dff941d9984e6cd
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-10-08 12:47:06 +0300

    Commit ECL - Embeddable Common Lisp package

---
+++ packages/ecl/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://common-lisp.net/project/ecl/
+TERMUX_PKG_DESCRIPTION="Embeddable Common-Lisp"
+TERMUX_PKG_LICENSE="LGPL-2.1+"
+TERMUX_PKG_VERSION=20.4.24
+TERMUX_PKG_SHA256=fb5d17bc3d6b8ad35f926e01d3b7f5730313bddc705bf36db640af5e672451a5
+TERMUX_PKG_SRCURL=https://gitlab.com/embeddable-common-lisp/ecl/-/archive/${TERMUX_PKG_VERSION}/ecl-${TERMUX_PKG_VERSION}.tar.bz2
+TERMUX_PKG_BUILD_IN_SRC=true
