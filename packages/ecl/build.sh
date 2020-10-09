4eb7d5c401134bbf757e949bcafb0bec201bcb88
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-10-09 13:51:37 +0300

    ecl: use TERMUX_PKG_LICENSE_FILE to specify the license

--- packages/ecl/build.sh
+++ packages/ecl/build.sh
@@ -1,13 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://common-lisp.net/project/ecl/
-TERMUX_PKG_DESCRIPTION="Embeddable Common-Lisp"
-TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_DESCRIPTION="Embeddable Common Lisp"
+TERMUX_PKG_LICENSE_FILE="./LICENSE"
 TERMUX_PKG_VERSION=20.4.24
 TERMUX_PKG_SRCURL=https://gitlab.com/embeddable-common-lisp/ecl/-/archive/${TERMUX_PKG_VERSION}/ecl-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=fb5d17bc3d6b8ad35f926e01d3b7f5730313bddc705bf36db640af5e672451a5
 TERMUX_PKG_MAINTAINER="Marin Atanasov Nikolov <dnaeon@gmail.com>"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_install_license() {
-    install -m 0644 "${TERMUX_PKG_SRCDIR}/LICENSE" \
-            "$TERMUX_PREFIX/share/doc/${TERMUX_PKG_NAME}/LICENSE"
-}
